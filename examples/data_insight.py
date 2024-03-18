147b5235e86e24d377f16d6f4c797be4a6218b80
Zhong Hao <zhonghao.austin@gmail.com>
2024-03-18 21:47:15 +0800

    insight data

---
+++ examples/data_insight.py
@@ -0,0 +1,100 @@
+#  Copyright (c) Microsoft Corporation.
+#  Licensed under the MIT License.
+"""
+Qlib provides two kinds of interfaces. 
+(1) Users could define the Quant research workflow by a simple configuration.
+(2) Qlib is designed in a modularized way and supports creating research workflow by code just like building blocks.
+
+The interface of (1) is `qrun XXX.yaml`.  The interface of (2) is script like this, which nearly does the same thing as `qrun XXX.yaml`
+"""
+import qlib
+from qlib.constant import REG_CN
+from qlib.utils import init_instance_by_config, flatten_dict
+from qlib.workflow import R
+from qlib.workflow.record_temp import SignalRecord, PortAnaRecord, SigAnaRecord
+from qlib.tests.data import GetData
+from qlib.tests.config import CSI300_BENCH, CSI300_GBDT_TASK
+
+import lightgbm as lgb
+
+# use default data
+provider_uri = "~/.qlib/qlib_data/cn_data"  # target_dir
+GetData().qlib_data(target_dir=provider_uri, region=REG_CN, exists_skip=True)
+qlib.init(provider_uri=provider_uri, region=REG_CN)
+
+model = init_instance_by_config(CSI300_GBDT_TASK["model"])
+
+
+import yaml
+
+model = """
+model:
+    class: RandomForestModel
+    module_path: qlib.contrib.model.random_forest
+dataset:
+    class: DatasetH
+    module_path: qlib.data.dataset
+    kwargs:
+        handler:
+            class: Alpha158
+            module_path: qlib.contrib.data.handler
+            kwargs:
+                start_time: 2008-01-01
+                end_time: 2020-08-01
+                fit_start_time: 2008-01-01
+                fit_end_time: 2014-12-31
+                instruments: csi300
+                infer_processors:
+                    - class: RobustZScoreNorm
+                      kwargs:
+                        fields_group: feature
+                        clip_outlier: true
+                    - class: Fillna
+                      kwargs:
+                        fields_group: feature
+                learn_processors:
+                    - class: DropnaLabel
+                    - class: CSRankNorm
+                      kwargs:
+                        fields_group: label
+        segments:
+            train: [2008-01-01, 2014-12-31]
+            valid: [2015-01-01, 2016-12-31]
+            test: [2017-01-01, 2020-08-01]        
+"""
+# save load config
+config = yaml.safe_load(model)
+dataset = init_instance_by_config(config["dataset"])
+
+rf_model_config = {
+    "class": "RandomForestModel",
+    "module_path": "qlib.contrib.model.random_forest",
+}
+
+rf = init_instance_by_config(rf_model_config)
+
+rf.fit(dataset)
+
+
+# NOTE: This line is optional
+# It demonstrates that the dataset can be used standalone.
+example_df = dataset.prepare("train")
+
+
+X = example_df.iloc[:, :-1]
+y = example_df.iloc[:, -1]
+
+# get feature importance
+lgb_model = lgb.LGBMRegressor()
+# lgb_model.fit(example_df.iloc[:, :-1], example_df.iloc[:, -1])
+lgb_model.fit(X, y)
+
+
+X = example_df.dropna(axis=1).iloc[:, :-1]
+y = example_df.dropna(axis=1).iloc[:, -1]
+
+
+from sklearn.ensemble import RandomForestRegressor
+
+rf = RandomForestRegressor()
+rf.fit(X, y)
